a4f70133fe5960b23c0c6d342de926cfd3d23298
김종권 <jongkwon.kim@42dot.com>
2021-11-07 20:43:05 +0900

    Implement deep link

--- ExWKWebView/WebViewController.swift
+++ ExWKWebView/WebViewController.swift
@@ -16,12 +16,29 @@ class WebViewController: BaseViewController {
     weak var delegate: WebViewDelegate?
     var url: URL
 
-    private lazy var webView: WKWebView = {
-        let webview = WKWebView(frame: .zero)
-        webview.allowsBackForwardNavigationGestures = true
-        webview.navigationDelegate = self
-        return webview
-    }()
+    private var webView: WKWebView!
+
+    private var headers: [String: String] {
+        let bundleVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
+        var header = ["Content-Type": "application/json"]
+        header["device-uuid"] = UUID().uuidString
+        header["device-os-version"] = UIDevice.current.systemVersion
+        header["device-device-manufacturer"] = "apple"
+        header["version"] = bundleVersion
+        return header
+    }
+
+    private var authCookie: HTTPCookie? {
+        let cookie = HTTPCookie(properties: [
+            .domain: "https://ios-development.tistory.com/",
+            .path: "748",
+            .name: "CID_AUTH",
+            .value: "test-access-token",
+            .maximumAge: 7200, // Cookie의 유효한 지속시간
+            .secure: "TRUE"
+        ])
+        return cookie
+    }
 
     init(url: URL, title: String) {
         self.url = url
@@ -35,10 +52,30 @@ class WebViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupWebView()
+        loadWebPage()
         addSubviews()
         makeConstraints()
         addBottomToolBar()
-        loadWebPage()
+    }
+
+    private func setupWebView() {
+        let configuration = WKWebViewConfiguration()
+        if let authCookie = authCookie {
+            let dataStore = WKWebsiteDataStore.nonPersistent()
+            dataStore.httpCookieStore.setCookie(authCookie)
+            configuration.websiteDataStore = dataStore
+        }
+
+        webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.allowsBackForwardNavigationGestures = true
+        webView.navigationDelegate = self
+    }
+
+    private func loadWebPage() {
+        var urlRequest = URLRequest(url: url)
+        headers.forEach { urlRequest.setValue($0.value, forHTTPHeaderField: $0.key) }
+        webView.load(urlRequest)
     }
 
     private func addSubviews() {
@@ -51,11 +88,6 @@ class WebViewController: BaseViewController {
         webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         webView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-    }
-
-    private func loadWebPage() {
-        let urlRequest = URLRequest(url: url)
-        webView.load(urlRequest)
     }
 
     override func didTapToolBarBackButton() {
@@ -81,5 +113,20 @@ extension WebViewController: WKNavigationDelegate {
     /// WKWebView에서 다른곳으로 이동된 후에 호출되는 메소드 (didCommit와 짝꿍)
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         print("hide loading indicator ...")
+    }
+
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+
+        if let url = navigationAction.request.url, url.scheme == "mailto" || url.scheme == "tel" {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+            // url이 mailto, tel인 경우, webView에서 열리지 않도록 .cancel
+            decisionHandler(.cancel)
+            return
+        }
+
+        // url이 네이티브에서 여는작업이 아닌 경우, webView에서 열리도록 .allow
+        decisionHandler(.allow)
     }
 }
